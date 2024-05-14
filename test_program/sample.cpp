1ca659d856a972b527e69da96494ab1f81effea7
Voleco <chenjingwei1991@gmail.com>
2024-05-14 15:46:05 -0600

    feat: add test code for win check with 1000,000 hands

--- test_program/sample.cpp
+++ test_program/sample.cpp
@@ -1,4 +1,8 @@
+
 #include <iostream>
+#include <chrono>
+#include <random>
+#include <algorithm>
 
 #include "../game/game_server.h"
 #include "../game/hand_evaluator.h"
@@ -23,5 +27,54 @@ void test1()
         {
             std::cout << "hand " << i << " is win\n";
         }
+    }
+
+    std::vector<hand_t> test_cases;
+    std::vector<card_t> all_cards;
+    for (int x = 0; x < 3; x++)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            card_t cur_card = 20 * x + i + 1;
+            for (int j = 0; j < 4; j++)
+                all_cards.push_back(cur_card);
+        }
+    }
+
+    int test_count = 1000000;
+
+    int count_win = 0;
+    std::vector<hand_t> hand_win;
+
+    std::random_device rd;
+    std::mt19937 rng(rd());
+
+    auto time1 = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < test_count; i++)
+    {
+        std::shuffle(all_cards.begin(), all_cards.end(), rng);
+        hand_t cur_hand = hand_t(all_cards.begin(), all_cards.begin() + 14);
+
+        if (he.is_Win(cur_hand) == true)
+        {
+            hand_win.push_back(cur_hand);
+            count_win++;
+        }
+    }
+
+    auto time2 = std::chrono::high_resolution_clock::now();
+
+    auto time_usage = std::chrono::duration_cast<std::chrono::milliseconds>(time2 - time1).count();
+    std::cout << "time usage: " << time_usage / 1000.0 << " s\n";
+
+    std::cout << count_win << " win out of " << test_count << "\n";
+
+    for (auto hand : hand_win)
+    {
+        std::sort(hand.begin(), hand.end());
+        for (auto item : hand)
+            std::cout << int(item) << " ";
+        std::cout << "\n";
     }
 }
